3b50e76ba4c121dbc95c275b748b154c6bea12cf
akshay294414 <akshay1082006@gmail.com>
2026-06-22 11:59:15 +0530

    feat: implement image upload functionality using Cloudinary

--- 1/utlity/file.uplade.js
+++ 1/utlity/file.uplade.js
@@ -1,1 +1,31 @@
-import multer from "multer"
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name:
+    process.env.CLOUDINARY_CLOUD_NAME,
+
+  api_key:
+    process.env.CLOUDINARY_API_KEY,
+
+  api_secret:
+    process.env.CLOUDINARY_API_SECRET,
+});
+
+
+export const uploadImage =
+async (req, res) => {
+
+  const result =
+    await cloudinary.uploader.upload(
+      req.file.path,
+      {
+        folder: "users",
+      }
+    );
+
+  res.json({
+    success: true,
+    imageUrl:
+      result.secure_url,
+  });
+};
